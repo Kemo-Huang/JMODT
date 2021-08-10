eval mAP for sustech dataset, fix 3d iou computation bugs

jmodt/detection/evaluation/eval.py
@@ -4,7 +4,7 @@ import numba
 import numpy as np
 
 from jmodt.detection.evaluation.rotate_iou import rotate_iou_gpu_eval
-
+from jmodt.utils import kitti_utils
 
 @numba.jit
 def get_thresholds(scores: np.ndarray, num_gt, num_sample_pts=41):
@@ -357,35 +357,73 @@ def calculate_iou_partly(gt_annos, dt_annos, metric, num_parts=50):
             dt_boxes = np.concatenate([a["bbox"] for a in dt_annos_part], 0)
             overlap_part = image_box_overlap(gt_boxes, dt_boxes)
         elif metric == 1:
+            # loc = np.concatenate(
+            #     [a["location"][:, [0, 2]] for a in gt_annos_part], 0)
+            # dims = np.concatenate(
+            #     [a["dimensions"][:, [0, 2]] for a in gt_annos_part], 0)
+            # rots = np.concatenate([a["rotation_y"] for a in gt_annos_part], 0)
+            # gt_boxes = np.concatenate(
+            #     [loc, dims, rots[..., np.newaxis]], axis=1)
+            # loc = np.concatenate(
+            #     [a["location"][:, [0, 2]] for a in dt_annos_part], 0)
+            # dims = np.concatenate(
+            #     [a["dimensions"][:, [0, 2]] for a in dt_annos_part], 0)
+            # rots = np.concatenate([a["rotation_y"] for a in dt_annos_part], 0)
+            # dt_boxes = np.concatenate(
+            #     [loc, dims, rots[..., np.newaxis]], axis=1)
+            # overlap_part = bev_box_overlap(gt_boxes, dt_boxes).astype(
+            #     np.float64)
             loc = np.concatenate(
-                [a["location"][:, [0, 2]] for a in gt_annos_part], 0)
+                [a["location"] for a in gt_annos_part], 0)
             dims = np.concatenate(
-                [a["dimensions"][:, [0, 2]] for a in gt_annos_part], 0)
+                [a["dimensions"][:, [1, 2, 0]] for a in gt_annos_part], 0)
             rots = np.concatenate([a["rotation_y"] for a in gt_annos_part], 0)
             gt_boxes = np.concatenate(
                 [loc, dims, rots[..., np.newaxis]], axis=1)
+            gt_corners = kitti_utils.boxes3d_to_corners3d(gt_boxes)
+
             loc = np.concatenate(
-                [a["location"][:, [0, 2]] for a in dt_annos_part], 0)
+                [a["location"] for a in dt_annos_part], 0)
             dims = np.concatenate(
-                [a["dimensions"][:, [0, 2]] for a in dt_annos_part], 0)
+                [a["dimensions"][:, [1, 2, 0]] for a in dt_annos_part], 0)
             rots = np.concatenate([a["rotation_y"] for a in dt_annos_part], 0)
             dt_boxes = np.concatenate(
                 [loc, dims, rots[..., np.newaxis]], axis=1)
-            overlap_part = bev_box_overlap(gt_boxes, dt_boxes).astype(
-                np.float64)
+            dt_corners = kitti_utils.boxes3d_to_corners3d(dt_boxes)
+            bev = kitti_utils.compute_iou(gt_corners, dt_corners, need_iou3d=False)
+            overlap_part = bev.astype(np.float64)
         elif metric == 2:
-            loc = np.concatenate([a["location"] for a in gt_annos_part], 0)
-            dims = np.concatenate([a["dimensions"] for a in gt_annos_part], 0)
+            # loc = np.concatenate([a["location"] for a in gt_annos_part], 0)
+            # dims = np.concatenate([a["dimensions"] for a in gt_annos_part], 0)
+            # rots = np.concatenate([a["rotation_y"] for a in gt_annos_part], 0)
+            # gt_boxes = np.concatenate(
+            #     [loc, dims, rots[..., np.newaxis]], axis=1)
+            # loc = np.concatenate([a["location"] for a in dt_annos_part], 0)
+            # dims = np.concatenate([a["dimensions"] for a in dt_annos_part], 0)
+            # rots = np.concatenate([a["rotation_y"] for a in dt_annos_part], 0)
+            # dt_boxes = np.concatenate(
+            #     [loc, dims, rots[..., np.newaxis]], axis=1)
+            # overlap_part = d3_box_overlap(gt_boxes, dt_boxes).astype(
+            #     np.float64)
+            loc = np.concatenate(
+                [a["location"] for a in gt_annos_part], 0)
+            dims = np.concatenate(
+                [a["dimensions"][:, [1, 2, 0]] for a in gt_annos_part], 0)
             rots = np.concatenate([a["rotation_y"] for a in gt_annos_part], 0)
             gt_boxes = np.concatenate(
                 [loc, dims, rots[..., np.newaxis]], axis=1)
-            loc = np.concatenate([a["location"] for a in dt_annos_part], 0)
-            dims = np.concatenate([a["dimensions"] for a in dt_annos_part], 0)
+            gt_corners = kitti_utils.boxes3d_to_corners3d(gt_boxes)
+
+            loc = np.concatenate(
+                [a["location"] for a in dt_annos_part], 0)
+            dims = np.concatenate(
+                [a["dimensions"][:, [1, 2, 0]] for a in dt_annos_part], 0)
             rots = np.concatenate([a["rotation_y"] for a in dt_annos_part], 0)
             dt_boxes = np.concatenate(
                 [loc, dims, rots[..., np.newaxis]], axis=1)
-            overlap_part = d3_box_overlap(gt_boxes, dt_boxes).astype(
-                np.float64)
+            dt_corners = kitti_utils.boxes3d_to_corners3d(dt_boxes)
+            bev, iou3d = kitti_utils.compute_iou(gt_corners, dt_corners, need_iou3d=True)
+            overlap_part = iou3d.astype(np.float64)
         else:
             raise ValueError("unknown metric")
         parted_overlaps.append(overlap_part)
@@ -441,7 +479,7 @@ def _prepare_data(gt_annos, dt_annos, current_class, difficulty):
 def eval_class(gt_annos,
                dt_annos,
                current_classes,
-               difficultys,
+               difficulties,
                metric,
                min_overlaps,
                compute_aos=False,
@@ -451,7 +489,7 @@ def eval_class(gt_annos,
         gt_annos: dict, must from get_label_annos() in kitti_evaluate.py
         dt_annos: dict, must from get_label_annos() in kitti_evaluate.py
         current_classes: list of int, 0: car, 1: pedestrian, 2: cyclist
-        difficultys: list of int. eval difficulty, 0: easy, 1: normal, 2: hard
+        difficulties: list of int. eval difficulty, 0: easy, 1: normal, 2: hard
         metric: eval type. 0: bbox, 1: bev, 2: 3d
         min_overlaps: float, min overlap. format: [num_overlap, metric, class].
         num_parts: int. a parameter for fast calculate algorithm
@@ -468,14 +506,14 @@ def eval_class(gt_annos,
     N_SAMPLE_PTS = 41
     num_minoverlap = len(min_overlaps)
     num_class = len(current_classes)
-    num_difficulty = len(difficultys)
+    num_difficulty = len(difficulties)
     precision = np.zeros(
         [num_class, num_difficulty, num_minoverlap, N_SAMPLE_PTS])
     recall = np.zeros(
         [num_class, num_difficulty, num_minoverlap, N_SAMPLE_PTS])
     aos = np.zeros([num_class, num_difficulty, num_minoverlap, N_SAMPLE_PTS])
     for m, current_class in enumerate(current_classes):
-        for l, difficulty in enumerate(difficultys):
+        for l, difficulty in enumerate(difficulties):
             rets = _prepare_data(gt_annos, dt_annos, current_class, difficulty)
             (gt_datas_list, dt_datas_list, ignored_gts, ignored_dets,
              dontcares, total_dc_num, total_num_valid_gt) = rets

jmodt/utils/sustech_utils.py
@@ -5,7 +5,7 @@ from jmodt.utils.pcd_utils import PointCloud
 
 
 @njit
-def euler_angle_to_rotate_matrix(theta, t=None):
+def euler_angle_to_rotate_matrix(theta):
     # Calculate rotation about x axis
     R_x = np.array([
         [1, 0., 0.],
@@ -24,24 +24,25 @@ def euler_angle_to_rotate_matrix(theta, t=None):
         [np.sin(theta[2]), np.cos(theta[2]), 0.],
         [0., 0., 1.]])
     R = np.dot(R_x, np.dot(R_y, R_z))
-
-    # Add translation if available
-    if t is not None:
-        t = t.reshape((-1, 1))
-        R = np.concatenate((R, t), axis=-1)
-        R = np.concatenate((R, np.array([0, 0, 0, 1]).reshape((1, -1))), axis=0)
     return R
 
 
-@njit
-def psr_to_corners(p, s, r):
+def psr_to_corners(psr: dict):
     """
-    :param p: position
-    :param s: scale
-    :param r: rotation
+    :param psr: a dict with keys: position, scale and rotation
     :return: (8, 3)
     """
-    trans_matrix = euler_angle_to_rotate_matrix(r, p)
+    p = np.array((psr['position']['x'], psr['position']['y'], psr['position']['z']))
+    s = np.array((psr['scale']['x'], psr['scale']['y'], psr['scale']['z']))
+    r = np.array((psr['rotation']['x'], psr['rotation']['y'], psr['rotation']['z']))
+    return psr_to_corners_jit(p, s, r)
+
+
+@njit
+def psr_to_corners_jit(p, s, r):
+    rotate_matrix = euler_angle_to_rotate_matrix(r)
+    p = p.reshape((-1, 1))
+    trans_matrix = np.concatenate((rotate_matrix, p), axis=-1)
     x = s[0] / 2
     y = s[1] / 2
     z = s[2] / 2
@@ -51,8 +52,34 @@ def psr_to_corners(p, s, r):
         -x, y, -z, 1, -x, -y, -z, 1,  # rear-left-bottom, rear-right-bottom
         -x, -y, z, 1, -x, y, z, 1,  # rear-right-top,   rear-left-top
     ]).reshape((-1, 4))
-    world_coord = np.dot(trans_matrix, np.transpose(local_coord))
+    world_coord = np.transpose(np.dot(trans_matrix, np.transpose(local_coord)))
     return world_coord
+
+
+def psr_to_kitti_box(psr):
+    """
+    :param psr: a dict with keys: position, scale and rotation
+    :return: (7,) [x,y,z,h,w,l,ry]
+    """
+    box = np.array((
+        -psr['position']['x'],
+        psr['scale']['z'] / 2 - psr['position']['z'],
+        -psr['position']['y'],
+        psr['scale']['z'],  # h
+        psr['scale']['y'],  # w
+        psr['scale']['x'],  # l
+        -psr['rotation']['z'] + (np.pi if psr['rotation']['z'] > 0 else - np.pi)
+    ))
+    return box
+
+
+def psr_to_kitti_alpha(psr):
+    x = -psr['position']['x']
+    z = -psr['position']['y']
+    ry = -psr['rotation']['z'] + (np.pi if psr['rotation']['z'] > 0 else - np.pi)
+    beta = np.arctan2(z, x)
+    alpha = -np.sign(beta) * np.pi / 2 + beta + ry
+    return alpha
 
 
 @njit
@@ -80,6 +107,47 @@ def proj_lidar_to_img(pts, extrinsic, intrinsic, img_shape):
     pts_2d = pts_2d[valid_mask]
 
     return pts_2d, valid_mask
+
+
+# @njit (matmul not supported)
+def proj_box_to_img(corners_3d, extrinsic, intrinsic, img_shape):
+    assert corners_3d.shape[1] == 8 and corners_3d.shape[2] == 3
+    corners_3d = np.concatenate((
+        corners_3d, np.ones((len(corners_3d), 8, 1), dtype=np.float32)
+    ), axis=-1)  # (N, 8, 4)
+    corners_3d = np.transpose(corners_3d, (0, 2, 1))  # (N, 4, 8)
+    corners_2d = np.matmul(extrinsic, corners_3d)  # (N, 3, 8)
+    corners_2d = np.matmul(intrinsic, corners_2d)  # (N, 3, 8)
+    depth_mask = corners_2d[:, 2, :] >= 0  # (N, 8)
+    depth_mask = np.all(depth_mask, axis=1)  # (N) all depth should be >= 0
+    corners_2d = np.transpose(corners_2d, (1, 2, 0))  # (3, 8, N)
+    corners_2d = corners_2d[:2] / corners_2d[2]  # (2, 8, N)
+    corners_2d = np.transpose(corners_2d, (2, 1, 0))  # (N, 8, 2) [W, H]
+    row_mask = np.logical_and(corners_2d[:, :, 0] >= 0, corners_2d[:, :, 0] < img_shape[1])  # 0 <= x < W
+    col_mask = np.logical_and(corners_2d[:, :, 1] >= 0, corners_2d[:, :, 1] < img_shape[0])  # 0 <= y < H
+    xy_mask = np.logical_and(row_mask, col_mask)  # (N, 8)
+    xy_mask = np.any(xy_mask, axis=1)  # (N) at least one corner should be in image
+
+    valid_mask = np.logical_and(depth_mask, xy_mask)  # (N)
+    corners_2d = corners_2d[valid_mask]  # (M, 8, 2)
+
+    min_xy = np.min(corners_2d, axis=1)  # (M, 2)
+    max_xy = np.max(corners_2d, axis=1)  # (M, 2)
+
+    boxes = np.hstack((min_xy, max_xy))  # (M, 4) [x1, y1, x2, y2]
+
+    img_boxes = np.hstack(
+        (np.clip(boxes[:, 0], 0, img_shape[1] - 1).reshape(-1, 1),
+         np.clip(boxes[:, 1], 0, img_shape[0] - 1).reshape(-1, 1),
+         np.clip(boxes[:, 2], 0, img_shape[1] - 1).reshape(-1, 1),
+         np.clip(boxes[:, 3], 0, img_shape[0] - 1).reshape(-1, 1))
+    )  # (M, 4) [x1, y1, x2, y2]
+
+    area1 = (img_boxes[:, 2] - img_boxes[:, 0]) * (img_boxes[:, 3] - img_boxes[:, 1])
+    area2 = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
+    truncation = (area2 - area1) / area2
+
+    return img_boxes, valid_mask, truncation
 
 
 def pcd_to_bin(in_path, out_path):

tools/test_sustech.py
@@ -11,19 +11,23 @@ import tqdm
 from torch.utils.data import DataLoader
 
 from jmodt.config import cfg
-from jmodt.detection.datasets.sustech_dataset import SUSTechDataset, save_sustech_format
+from jmodt.detection.datasets.sustech_dataset import SUSTechDataset, save_sustech_format, convert_sustech_label_to_kitti
+from jmodt.detection.evaluation.evaluate import evaluate as evaluate_detection
 from jmodt.detection.modeling.point_rcnn import PointRCNN
 from jmodt.ops.iou3d import iou3d_utils
 from jmodt.tracking import tracker
-from jmodt.utils import train_utils, kitti_utils, sustech_utils
+from jmodt.utils import train_utils, kitti_utils, sustech_utils, object3d
 from jmodt.utils.bbox_transform import decode_bbox_target
 
 parser = argparse.ArgumentParser(description="arg parser")
-parser.add_argument('--data_root', type=str, default='E://sustech-data/2021-07-07/dataset_2hz',
+parser.add_argument('--data_root', type=str, default='E://sustech-data/2021-06-25-06-56-55/dataset',
                     help='the ground truth data root')
-parser.add_argument('--det_output', type=str, default='output/det/2021-07-07/',
+parser.add_argument('--det_output', type=str, default='output/det/2021-06-25/',
                     help='the detection output directory')
-parser.add_argument('--trk_output', type=str, default='output/trk/2021-07-07/', help='the tracking output directory')
+parser.add_argument('--trk_output', type=str, default='output/trk/2021-06-25/',
+                    help='the tracking output directory')
+parser.add_argument('--kitti_output', type=str, default='output/kitti_det/2021-06-25/',
+                    help='the KITTI format detection output directory')
 parser.add_argument('--ckpt', type=str, default='checkpoints/jmodt.pth', help='the pretrained model path')
 parser.add_argument('--tag', type=str, default='all', help='the tag for tracking results')
 parser.add_argument('--hungarian', action='store_true', help='whether to use hungarian algorithm')
@@ -211,7 +215,7 @@ def joint_detection_and_tracking(logger, detection=True, tracking=True, fps=10):
                 for d in range(len(car_objects)):
                     psr = car_objects[d]['psr']
                     boxes_3d[d, 0] = -psr['position']['x']
-                    boxes_3d[d, 1] = psr['scale']['y'] / 2 - psr['position']['z']
+                    boxes_3d[d, 1] = psr['scale']['z'] / 2 - psr['position']['z']
                     boxes_3d[d, 2] = -psr['position']['y']
                     boxes_3d[d, 3] = psr['scale']['z']
                     boxes_3d[d, 4] = psr['scale']['y']
@@ -271,6 +275,38 @@ def convert_pcd_to_bin(logger):
     logger.info('==> Done')
 
 
+def eval_detection(logger, convert_gt=True):
+    calib_dir = os.path.join(args.data_root, 'calib')
+    label_kitti_dir = os.path.join(args.data_root, 'label_kitti')
+    camera_list = ['rear', 'rear_left', 'rear_right', 'front', 'front_left', 'front_right']
+
+    if convert_gt:
+        label_dir = os.path.join(args.data_root, 'label')
+        logger.info(f'=> Converting gt to {label_kitti_dir}')
+        convert_sustech_label_to_kitti(label_dir, label_kitti_dir, calib_dir, camera_list, score=False)
+
+    txt_output_dir = os.path.join(args.trk_output, 'all')
+    logger.info(f'=> Converting output to {args.kitti_output}')
+    convert_sustech_label_to_kitti(txt_output_dir, args.kitti_output, calib_dir, camera_list, score=True)
+
+    logger.info('=> Evaluating output')
+    for camera in camera_list:
+        logger.info(f'================== {camera} ==================')
+        kitti_txt_input_dir = os.path.join(label_kitti_dir, camera)
+        kitti_txt_output_dir = os.path.join(args.kitti_output, camera)
+        image_idx_list = sorted([f[:-4] for f in os.listdir(kitti_txt_input_dir)])
+
+        ap_result_str, ap_dict = evaluate_detection(kitti_txt_input_dir, kitti_txt_output_dir,
+                                                    image_idx_list=image_idx_list,
+                                                    current_class=0)
+        logger.info(ap_result_str)
+
+
+
+def eval_tracking(logger):
+    pass
+
+
 def main():
     logger = logging.getLogger(__name__)
     logger.setLevel(level=logging.INFO)
@@ -281,10 +317,10 @@ def main():
     ch.setLevel(level=logging.INFO)
     logger.addHandler(ch)
 
-    convert_pcd_to_bin(logger)
-
     # start
-    joint_detection_and_tracking(logger, detection=True, tracking=True, fps=2)
+    # convert_pcd_to_bin(logger)
+    # joint_detection_and_tracking(logger, detection=True, tracking=True, fps=2)
+    eval_detection(logger, convert_gt=True)
 
 
 if __name__ == '__main__':