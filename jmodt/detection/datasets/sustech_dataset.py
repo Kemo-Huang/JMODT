049dfe23b1fade25d924862bafdb684d03b6e5d1
Kemo-Huang <12032943@mail.sustech.edu.cn>
2021-09-15 14:56:32 +0800

    update

---
+++ jmodt/detection/datasets/sustech_dataset.py
@@ -0,0 +1,188 @@
+import json
+import os
+
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+
+from jmodt.utils.sustech_utils import proj_lidar_to_img, psr_to_corners
+
+
+class SUSTechDataset(Dataset):
+    def __init__(self, root_dir, camera):
+        self.root_dir = root_dir
+        self.camera = camera
+
+        self.npoints = 16384
+        self.mean = [0.485, 0.456, 0.406]
+        self.std = [0.229, 0.224, 0.225]
+
+        self.num_class = 2  # only cars
+        self.img_shape = (1536, 2048, 3)
+
+        lidar_sample_list = sorted(
+            [f[:-4] for f in os.listdir(os.path.join(root_dir, 'lidar_bin'))]
+        )
+        camera_sample_list = sorted(
+            [f[:-4] for f in os.listdir(os.path.join(root_dir, 'camera', camera))]
+        )
+        self.sample_id_list = [s for s in lidar_sample_list if s in camera_sample_list]
+
+    def __len__(self):
+        return len(self.sample_id_list)
+
+    def __getitem__(self, idx):
+        sample_id = self.sample_id_list[idx]
+        return self.get_sample_dict(sample_id)
+
+    def get_lidar(self, frame):
+        file_path = os.path.join(self.root_dir, 'lidar_bin', f'{frame}.bin')
+        assert os.path.exists(file_path), file_path
+        return np.fromfile(file_path, dtype=np.float32).reshape(-1, 4)
+
+    def get_image(self, frame):
+        file_path = os.path.join(self.root_dir, 'camera', self.camera, f'{frame}.jpg')
+        assert os.path.exists(file_path), file_path
+        img = Image.open(file_path).convert('RGB')
+        img = np.array(img).astype(np.float32)
+        assert img.shape == self.img_shape
+        img /= 255.0
+        img -= self.mean
+        img /= self.std
+        return img
+
+    def get_calib(self):
+        file_path = os.path.join(self.root_dir, 'calib', 'camera', f'{self.camera}.json')
+        assert os.path.exists(file_path), file_path
+        with open(file_path, 'r') as f:
+            calib = json.load(f)
+        extrinsic = np.array(calib['extrinsic'], dtype=np.float32).reshape(4, 4)[:3, :]
+        intrinsic = np.array(calib['intrinsic'], dtype=np.float32).reshape(3, 3)
+        return extrinsic, intrinsic
+
+    def sample_points(self, pc):
+        # generate inputs
+        if self.npoints < len(pc):
+            # keep far points, sample near points
+            pts_depth = pc[:, 0]
+            pts_near_flag = pts_depth < 40.0
+            far_indices_choice = np.argwhere(pts_near_flag == 0).flatten()
+            near_indices = np.argwhere(pts_near_flag == 1).flatten()
+            near_indices_choice = np.random.choice(near_indices, self.npoints - len(far_indices_choice), replace=False)
+            choice = np.concatenate((near_indices_choice, far_indices_choice), axis=0) \
+                if len(far_indices_choice) > 0 else near_indices_choice
+            np.random.shuffle(choice)
+        else:
+            choice = np.arange(0, len(pc), dtype=np.int32)
+            while self.npoints > len(choice):
+                extra_choice = np.random.choice(choice, self.npoints % len(choice), replace=False)
+                choice = np.concatenate((choice, extra_choice), axis=0)
+            np.random.shuffle(choice)
+        return choice
+
+    @staticmethod
+    def get_pc_in_range(pc, xy_dist=70, z_range=(-3, 1)):
+        horizontal_distances = np.sqrt(np.square(pc[:, 0]) + np.square(pc[:, 1]))
+        xy_mask = horizontal_distances <= xy_dist
+        z_mask = np.logical_and(pc[:, 2] >= z_range[0], pc[:, 2] <= z_range[1])
+        return pc[np.logical_and(xy_mask, z_mask)]
+
+    def get_sample_dict(self, frame):
+        """
+        :return:
+            sample_id
+            img
+            pts_xy
+            pts_input
+            gt_boxes3d
+            gt_tid
+        """
+        pc = self.get_lidar(frame)
+        pts_xyz = pc[:, :3]
+        # intensity = pc[:, 3]
+        img = self.get_image(frame)
+        img_shape = img.shape[:2]
+        extrinsic, intrinsic = self.get_calib()
+
+        pts_xyz = self.get_pc_in_range(pts_xyz)
+        pts_xy, valid_mask = proj_lidar_to_img(pts_xyz, extrinsic, intrinsic, img_shape)
+        pts_xyz = pts_xyz[valid_mask]
+
+        choice = self.sample_points(pts_xyz)
+        pts_xyz = pts_xyz[choice]
+        pts_xy = pts_xy[choice]
+
+        # normalize xy to [-1,1]
+        pts_xy[:, 0] = pts_xy[:, 0] / (self.img_shape[1] - 1.0) * 2.0 - 1.0
+        pts_xy[:, 1] = pts_xy[:, 1] / (self.img_shape[0] - 1.0) * 2.0 - 1.0
+
+        # change view
+        if 'rear' in self.camera:
+            pts_xyz[:, 1] *= -1
+
+        # change coordinates
+        # sustech: x left, y rear, z up
+        # kitti: x right, y down, z front
+        pts_xyz = pts_xyz[:, [0, 2, 1]]
+        pts_xyz *= -1
+
+        sample_dict = {
+            'sample_id': frame,
+            'img': img,
+            'pts_xy': pts_xy,
+            'pts_input': pts_xyz
+        }
+
+        return sample_dict
+
+    @staticmethod
+    def collate_batch(batch):
+        batch_size = batch.__len__()
+        ans_dict = {}
+        for key in batch[0].keys():
+            if isinstance(batch[0][key], np.ndarray):
+                if batch_size == 1:
+                    ans_dict[key] = batch[0][key][np.newaxis, ...]
+                else:
+                    ans_dict[key] = np.concatenate([batch[k][key][np.newaxis, ...] for k in range(batch_size)],
+                                                   axis=0)
+            else:
+                ans_dict[key] = [batch[k][key] for k in range(batch_size)]
+                if isinstance(batch[0][key], int):
+                    ans_dict[key] = np.array(ans_dict[key], dtype=np.int32)
+                elif isinstance(batch[0][key], float):
+                    ans_dict[key] = np.array(ans_dict[key], dtype=np.float32)
+        return ans_dict
+
+
+def save_sustech_format(sample_id, bbox3d, score, txt_output_dir,
+                        feat=None, feat_output_dir=None):
+    bbox3d = bbox3d.astype(np.float64)
+    score = score.astype(np.float64).flatten()
+    output_file = os.path.join(txt_output_dir, f'{sample_id}.json')
+    output_list = [{
+        'psr': {
+            'position': {
+                'x': -bbox3d[k, 0],
+                'y': -bbox3d[k, 2],
+                'z': -bbox3d[k, 1] + bbox3d[k, 4] / 2
+            },
+            'scale': {
+                'x': bbox3d[k, 5],  # l
+                'y': bbox3d[k, 4],  # w
+                'z': bbox3d[k, 3]  # h
+            },
+            'rotation': {
+                'x': 0,
+                'y': 0,
+                'z': np.pi - bbox3d[k, 6]
+            }
+        },
+        'obj_type': 'Car',
+        'score': score[k]
+    } for k in range(bbox3d.shape[0])]
+    with open(output_file, 'w') as f:
+        json.dump(output_list, f)
+    if feat is not None:
+        output_file = os.path.join(feat_output_dir, f'{sample_id}.npy')
+        np.save(output_file, feat.astype(np.float32))
---
+++ jmodt/utils/sustech_utils.py
@@ -0,0 +1,112 @@
+import os
+from multiprocessing.pool import Pool
+
+import numpy as np
+import tqdm
+from numba import njit
+
+from jmodt.utils.pcd_utils import PointCloud
+
+
+@njit
+def euler_angle_to_rotate_matrix(theta, t=None):
+    # Calculate rotation about x axis
+    R_x = np.array([
+        [1, 0., 0.],
+        [0., np.cos(theta[0]), -np.sin(theta[0])],
+        [0., np.sin(theta[0]), np.cos(theta[0])]
+    ])
+    # Calculate rotation about y axis
+    R_y = np.array([
+        [np.cos(theta[1]), 0., np.sin(theta[1])],
+        [0., 1., 0.],
+        [-np.sin(theta[1]), 0., np.cos(theta[1])]
+    ])
+    # Calculate rotation about z axis
+    R_z = np.array([
+        [np.cos(theta[2]), -np.sin(theta[2]), 0.],
+        [np.sin(theta[2]), np.cos(theta[2]), 0.],
+        [0., 0., 1.]])
+    R = np.dot(R_x, np.dot(R_y, R_z))
+
+    # Add translation if available
+    if t is not None:
+        t = t.reshape((-1, 1))
+        R = np.concatenate((R, t), axis=-1)
+        R = np.concatenate((R, np.array([0, 0, 0, 1]).reshape((1, -1))), axis=0)
+    return R
+
+
+@njit
+def psr_to_corners(p, s, r):
+    """
+    :param p: position
+    :param s: scale
+    :param r: rotation
+    :return: (8, 3)
+    """
+    trans_matrix = euler_angle_to_rotate_matrix(r, p)
+    x = s[0] / 2
+    y = s[1] / 2
+    z = s[2] / 2
+    local_coord = np.array([
+        x, y, -z, 1, x, -y, -z, 1,  # front-left-bottom, front-right-bottom
+        x, -y, z, 1, x, y, z, 1,  # front-right-top,   front-left-top
+        -x, y, -z, 1, -x, -y, -z, 1,  # rear-left-bottom, rear-right-bottom
+        -x, -y, z, 1, -x, y, z, 1,  # rear-right-top,   rear-left-top
+    ]).reshape((-1, 4))
+    world_coord = np.dot(trans_matrix, np.transpose(local_coord))
+    return world_coord
+
+
+@njit
+def proj_lidar_to_img(pts, extrinsic, intrinsic, img_shape):
+    """
+    Project lidar points to image, the points outside the image are discarded.
+    :param pts: (N, 3) xyz data
+    :param extrinsic: (3, 4)
+    :param intrinsic: (3, 3)
+    :param img_shape: H, W
+    :return: pts_2d: (M, 2), valid_mask: (N,)
+    """
+    assert pts.shape[1] == 3
+    pts = np.hstack((pts, np.ones((len(pts), 1), dtype=np.float32)))
+    pts = np.transpose(pts)  # (4, N)
+    pts_2d = np.dot(extrinsic, pts)  # (3, N)
+    # rect matrix shall be applied here, for kitti
+    pts_2d = np.dot(intrinsic, pts_2d)  # (3, N)
+    depth_mask = pts_2d[2, :] >= 0
+    pts_2d = np.transpose(pts_2d[0:2, :] / pts_2d[2, :])  # (N, 2), W, H
+    row_mask = np.logical_and(pts_2d[:, 0] >= 0, pts_2d[:, 0] < img_shape[1])  # 0 <= x < W
+    col_mask = np.logical_and(pts_2d[:, 1] >= 0, pts_2d[:, 1] < img_shape[0])  # 0 <= y < H
+    xy_mask = np.logical_and(row_mask, col_mask)
+    valid_mask = np.logical_and(xy_mask, depth_mask)  # depth >= 0
+    pts_2d = pts_2d[valid_mask]
+
+    return pts_2d, valid_mask
+
+
+def pcd_to_bin(in_path, out_path):
+    pc = PointCloud(in_path)
+    pts = pc.data
+    pts.tofile(out_path)
+
+
+def main():
+    data_root = '/media/kemo/Kemo/sustech-data/2021-06-25-06-56-55/dataset_10hz'
+    os.makedirs(os.path.join(data_root, 'lidar_bin'), exist_ok=True)
+
+    arguments = []
+    for sustech_lidar in os.listdir(os.path.join(data_root, 'lidar')):
+        frame = sustech_lidar[:-4]
+        in_path = os.path.join(data_root, 'lidar', sustech_lidar)
+        out_path = os.path.join(data_root, 'lidar_bin', f'{frame}.bin')
+        arguments.append((in_path, out_path))
+
+    with Pool(8) as pool:
+        for _ in tqdm.tqdm(pool.starmap(pcd_to_bin, arguments), total=len(arguments)):
+            pass
+
+
+if __name__ == '__main__':
+    main()
